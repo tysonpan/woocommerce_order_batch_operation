bf3b77465dca7b16c12588f174f825a16bf77af4
tysonpan <pantaishen@gmail.com>
2014-06-28 20:01:33 +0800

    complete时加入运单号逻辑，并行执行改为串行执行

--- order_operation.js
+++ order_operation.js
@@ -9,10 +9,21 @@ function $(e){
     return jQuery(e);
 }
 
-Array.prototype.contains = function (element) { //利用Array的原型prototype点出一个我想要封装的方法名contains
+//自定义搜索订单号是否匹配的方法
+Array.prototype.contain_order_id = function (order_id) {
     for (var i = 0; i < this.length; i++) {
-        if (this[i] == element) { //如果数组中某个元素和你想要测试的元素对象element相等，则证明数组中包含这个元素，返回true
+        if (this[i][0] == order_id) { //如果数组中某个元素和你想要测试的元素对象element相等，则证明数组中包含这个元素，返回true
             return true;
+        }
+    }
+    return false;
+}
+
+//根据订单号找出对应的运单号
+Array.prototype.getTrackingNumber = function(order_id){
+    for (var i = 0; i < this.length; i++) {
+        if (this[i][0] == order_id) { //如果数组中某个元素和你想要测试的元素对象element相等，则证明数组中包含这个元素，返回true
+            return this[i][1];
         }
     }
     return false;
@@ -30,8 +41,8 @@ var elHtml = '<div id="operate_win">' +
     '<input type="text" class="order-id-input" id="order_id_list">' +
     '<p class="tips">input the order id list,separated by the comma.</p>' +
     '<div class="btn-container">' +
-    '<button class="processing-btn operate-btn" action="processing">processing</button>' +
-    '<button class="complete-btn operate-btn" action="complete">complete</button>' +
+//    '<button class="processing-btn operate-btn" action="processing">processing</button>' +
+    '<button class="complete-btn operate-btn" action="complete">complete and send tracking number</button>' +
     '</div>' +
     '<div class="status-list-container">' +
         '<ul class="status-list" id="operate_status_list">' +
@@ -42,58 +53,65 @@ var elHtml = '<div id="operate_win">' +
 $('body').append(elHtml);
 
 
-function order_operate(order_id_list,action){
-    var $operate_status_list = $('#operate_status_list');
-    $operate_status_list.empty();
+//全局变量
+var order_id_list;
+var i;
+var tr_list;
+var security;
+var $operate_status_list;
 
-    $('#the-list').children('tr').each(function(){
-        var order_id = $(this).children('.order_title').find('a strong').html().replace('#','');
-        if(order_id == '') return;
-        if(order_id_list.contains(order_id)){
-            if(action == 'processing'){
-                var $processing_button = $(this).find('a.processing');
-                if($processing_button.length>0){
-                    $operate_status_list.append('<li><span class="order-id">#'+order_id+'</span><i class="loading" id="i_status'+order_id+'"></i></li>');
-                    var ajax_link = $processing_button.attr('href');
+
+function complete_order(){
+    if(i > tr_list.length-1) return;
+    var $tr = $(tr_list[i]);
+    var order_id = $tr.find('a strong').html().replace('#','');
+    var post_id = $tr.attr('id').split('-')[1];
+    if(order_id == '') return;
+    if(order_id_list.contain_order_id(order_id)){
+        var $complete_button = $tr.find('a.complete');
+        if($complete_button.length>0){
+            $operate_status_list.append('<li><span class="order-id">#'+order_id+'</span><i class="loading" id="complete_status'+order_id+'"></i><i class="loading" id="note_status'+order_id+'"></i></li>');
+            var ajax_link = $complete_button.attr('href');
+            jQuery.ajax({
+                url : ajax_link,
+                type: 'GET',
+                dataType: 'text',
+                success: function(){
+                    //complete the order
+                    $('#complete_status'+order_id).removeClass('loading').addClass('ok');
+                    $complete_button.remove();
+                    //send tracking number
                     jQuery.ajax({
-                        url : ajax_link,
-                        type: 'GET',
+                        url : 'http://' + document.domain + '/wp-admin/admin-ajax.php',
+                        type: 'POST',
                         dataType: 'text',
+                        data:{action:'woocommerce_add_order_note',post_id:post_id,note:order_id_list.getTrackingNumber(order_id),note_type:'customer',security:security},
                         success: function(){
-                            $('#i_status'+order_id).removeClass('loading').addClass('ok');
-                            $processing_button.remove();
+                            $('#note_status'+order_id).removeClass('loading').addClass('ok');
+                            //next order
+                            i++;
+                            complete_order();
                         },
                         error: function(){
-                            $('#i_status'+order_id).removeClass('loading').addClass('fail');
+                            $('#note_status'+order_id).removeClass('loading').addClass('fail');
                         }
                     });
+                },
+                error: function(){
+                    $('#complete_status'+order_id).removeClass('loading').addClass('fail');
                 }
-            }
-            else if(action == 'complete'){
-                var $complete_button = $(this).find('a.complete');
-                if($complete_button.length>0){
-                    $operate_status_list.append('<li><span class="order-id">#'+order_id+'</span><i class="loading" id="i_status'+order_id+'"></i></li>');
-                    var ajax_link = $complete_button.attr('href');
-                    jQuery.ajax({
-                        url : ajax_link,
-                        type: 'GET',
-                        dataType: 'text',
-                        success: function(){
-                            $('#i_status'+order_id).removeClass('loading').addClass('ok');
-                            $complete_button.remove();
-                        },
-                        error: function(){
-                            $('#i_status'+order_id).removeClass('loading').addClass('fail');
-                        }
-                    });
-                }
-            }
+            });
         }
-    });
+    }
 }
 
 $('#operate_win .operate-btn').click(function(){
-    var order_id_arr = $('#order_id_list').val().split(',');
-    var action = $(this).attr('action');
-    order_operate(order_id_arr,action);
+    order_id_list = eval($('#order_id_list').val());
+    i=0;
+    tr_list = $('#the-list').children('tr');
+    security = woocommerce_admin_meta_boxes.add_order_note_nonce;
+    $operate_status_list = $('#operate_status_list');
+    $operate_status_list.empty();
+
+    complete_order();
 });
